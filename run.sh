ce4a01c768996094c07ea280363e6d6cab3be124
Craig Cook <craig.cook@code.red>
2017-09-12 11:06:06 +0100

    Added nohup

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ rm -rf master.zip master MovieService-master
 curl -O -L https://github.com/BoyCook/MovieService/archive/master.zip
 unzip master.zip
 cd MovieService-master
-mvn clean package && java -jar target/movie-service.jar --server.port=5000 -Djava.awt.headless=true
+mvn clean package
+nohup java -jar target/movie-service.jar --server.port=5000 -Djava.awt.headless=true &
 cd ../
 ./gradlew
