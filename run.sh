20b5d7050536c1dedb7775710a69f7be43ae4cae
Craig Cook <craig.cook@code.red>
2017-09-12 11:19:05 +0100

    Nohup spring boot

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-rm -rf master.zip master MovieService-master
+rm -rf master.zip master MovieService-master run.pid
 curl -O -L https://github.com/BoyCook/MovieService/archive/master.zip
 unzip master.zip
 cd MovieService-master
 mvn clean package
-nohup java -jar target/movie-service.jar --server.port=5000 -Djava.awt.headless=true &
+nohup java -jar target/movie-service.jar --server.port=5000 > /dev/null 2>&1 & echo $! > run.pid
 cd ../
 ./gradlew
+kill -9 `cat run.pid`
